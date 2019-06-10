be6062cfd2ecebc89882bf3ad32c7f58b44bba7c
zaets39 <katezytchuk24@gmail.com>
2019-06-10 16:13:27 +0300

    all listings from first chapter (not tested)

--- app/src/main/java/a24zaets39/org/chapter_1/Start.java
+++ app/src/main/java/a24zaets39/org/chapter_1/Start.java
@@ -2,14 +2,41 @@ package a24zaets39.org.chapter_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/*This app called GeoQuiz.
+It checks how well the user knows the geography.
+The user answers the question by tapping the True or False button,
+and GeoQuiz reports the result to him.*/
 
 public class Start extends AppCompatActivity
 {
-
+    private Button mTrueButton;
+    private Button mFalseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(Start.this,R.string.correct_tast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(Start.this,R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
