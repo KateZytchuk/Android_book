e29f6711af2c9b474326fa05885dd527eec26d04
zaets39 <katezytchuk24@gmail.com>
2019-06-10 14:57:28 +0300

    project created

---
+++ app/src/main/java/a24zaets39/org/chapter_1/Start.java
@@ -0,0 +1,15 @@
+package a24zaets39.org.chapter_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
